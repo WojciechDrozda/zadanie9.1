7d3e7a598754fc98153c4cd54a47b42ea730f079
Wojciech Drozda <wojciech.drozdaa@gmail.com>
2017-06-20 17:57:35 +0200

    Test

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 function getTriangleArea(a, h) {
-  var triangleArea;
   if (a>0 && h>0) {
-    triangArea = a*h/2;
-  return triangArea;
-  } else 
+	return a*h/2;
+  } 
     console.log('Błędne dane');
   }
 console.log( getTriangleArea(10,6) )
 var triangle1Area = getTriangleArea(130, 15);
 var triangle2Area = getTriangleArea(10, 105);
 var triangle3Area = getTriangleArea(105, 15);
-console.log('Pola trojkatow to : ' + triangle1Area + ', ' + triangle2Area + ', ' + triangle3Area )
+console.log('Pola trojkatow to : ' + triangle1Area + ', ' + triangle2Area + ', ' + triangle3Area );
